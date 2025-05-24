5ec8cf2f883c14d91f56c4d22576e1bfac60ffc6
David Kouril <david.kouril@gmail.com>
2025-05-24 12:08:48 -0400

    new page

---
+++ src/routes/work/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>work.</title>
+	<!--<meta
+		name="description"
+		content="This is where the description goes for SEO"
+	/>-->
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<div id="container">
+	<h2>work.</h2>
+	<h3>postdoc work.</h3>
+	<ul>
+		<li>gosling integration</li>
+		<li>chromospace</li>
+		<li>chromospyce</li>
+	</ul>
+
+	<h3>phd work.</h3>
+	<h3>undergraduate projects.</h3>
+</div>
+
+<style>
+	* {
+		font-family: "Dosis", sans-serif;
+	}
+
+	/*h2 {*/
+	/*	margin-top: 0;*/
+	/*}*/
+
+	#container {
+		width: 100%;
+		max-width: 800px;
+		font-size: 110%;
+	}
+</style>
